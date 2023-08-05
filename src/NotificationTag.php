b0e79e251e43da840a25783352be9b180cdd31f2
Jeroen el Hahaoui <jeroen.el.hahaoui@maxict.nl>
2023-08-06 00:08:50 +0200

    Update Notification tag value

--- src/NotificationTag.php
+++ src/NotificationTag.php
@@ -2,23 +2,29 @@
 
 namespace Microit\DashboardNotifications;
 
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Log;
 use Microit\DashboardNotifications\Models\NotificationTag as NotificationMTagModel;
 
 class NotificationTag
 {
-    protected NotificationMTagModel $notificationTagModel;
+    protected ?NotificationMTagModel $notificationTagModel;
 
     public function __construct(public readonly string $module, public readonly string $name, public readonly string $type, public readonly ?string $model = null)
     {
         /** @var ?NotificationMTagModel $notificationTagModel */
         $notificationTagModel = NotificationMTagModel::where('module', $this->module)->where('name', $this->name)->first();
         if (is_null($notificationTagModel)) {
-            $notificationTagModel = NotificationMTagModel::create([
-                'module' => $this->module,
-                'name' => $this->name,
-                'type' => $this->type,
-                'model' => $this->model,
-            ]);
+            try {
+                $notificationTagModel = NotificationMTagModel::create([
+                    'module' => $this->module,
+                    'name' => $this->name,
+                    'type' => $this->type,
+                    'model' => $this->model,
+                ]);
+            } catch (QueryException $exception) {
+                Log::error($exception->getMessage());
+            }
         }
 
         $this->notificationTagModel = $notificationTagModel;
